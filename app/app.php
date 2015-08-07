ccbd10e414cc459f4cab3f7e10061b23f7f8e4aa
Joe <joe.hartman@yahoo.com>
2015-08-07 12:02:50 -0700

    Set up contact form, allow add to list of contacts

--- app/app.php
+++ app/app.php
@@ -8,8 +8,23 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    $app->get("/", function() {
-        return "Hello friend!";
+    session_start();
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('contacts.html.twig', array('contacts' => Contact::getAll()));
+    });
+
+    $app->post("/contacts", function() use ($app) {
+        $contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
+        $contact->save();
+        return $app['twig']->render('create_contact.html.twig', array('new_contact' => $contact));
+    });
+
+    $app->post("/delete_contacts", function() use ($app) {
+
     });
 
     return $app;
